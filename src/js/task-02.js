973579d44b1a864848be07aa4a5b578f0569a2d6
Volodymyr-Musaielian <121056577+Volodymyr-Musaielian@users.noreply.github.com>
2023-04-27 14:48:28 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+ingredients.forEach((item) => {
+  const listItems = document.createElement("li");
+
+  listItems.textContent = [item];
+  listItems.classList.add("item");
+  ingredientsList.append(listItems);
+});
+
+console.log(ingredientsList);
