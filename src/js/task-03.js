2d0204ebbb96fd760fb5fe2270e4f365dc3dff34
Volodymyr-Musaielian <121056577+Volodymyr-Musaielian@users.noreply.github.com>
2023-04-27 21:49:31 +0300

    update task-03

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -16,6 +16,6 @@ const images = [
 const gallery = document.querySelector(".gallery");
 
 const markup = images
-  .map(({ url, alt }) => `<li><img src=${url} alt=${alt}></li>`)
+  .map(({ url, alt }) => `<li><img src='${url}' alt='${alt}'></li>`)
   .join("");
 gallery.insertAdjacentHTML("afterbegin", markup);
